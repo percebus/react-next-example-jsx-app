810c6aab61f60fdfa8bc8694ec087f0b0ce45389
JC Guerrero <JCGuerrero@MicroSoft.com>
2022-06-23 11:29:33 -0500

    feat(introduction)

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import '../styles/global.css'
+
+function App ({ Component, pageProps }) {
+  return <Component {...pageProps} />
+} export default App
--- pages/posts/first.jsx
+++ pages/posts/first.jsx
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
+import Layout from '../../components/layout/Layout'
 
 function FirstPost () {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
+
+        {/* FIXME: "Error: next/script shouldn't be used inside next/head" */}
+        <Script
+          src='https://connect.facebook.net/en_US/sdk.js'
+          strategy='lazyOnload'
+          onLoad={() =>
+            console.log('script loaded correctly, window.FB has been populated')}
+        />
       </Head>
       <h1>First Post</h1>
       <h2>
@@ -13,8 +23,6 @@ function FirstPost () {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
-}
-
-export default FirstPost
+} export default FirstPost
