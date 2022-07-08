b13496101f9423bf765cfbfcaaa0861cf45ab5c1
JC Guerrero <JCystems@gMail.com>
2022-07-08 11:09:44 -0500

    fix(Layout.jsx)

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image' // FIXME "next export" complains
+// import Image from 'next/image' // FIXME "next export" complains
 import styles from './layout.module.css'
 import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
